5b3a66a7cb672dad4af2502aaf8c8550619c392c
Milan Tucakovic <102631211+Tucaa@users.noreply.github.com>
2026-01-19 02:43:15 +0100

    Setting up windowing
    Added some features for creating timelines for ddos attack patterns.  The core.clj file for dataset generation has been updated. It remains to fix some small bugs with writing to the .csv file

--- src/ddos/core.clj
+++ src/ddos/core.clj
@@ -9,31 +9,101 @@
 
 ;; Po potrebi korigovati parametre (za pocetak je stavljeno 1k uzoraka svakog patterna)
 ;; Vidi da li ces ovde da mesas saobracaj ili ces to uraditi u drugim funkcijama u normal.clj i attack.clj
-(defn -main [& _]
-  (let [start-ts (System/currentTimeMillis)
-        window-ms 5000
-        samples
-        (->> (concat
-              ;;  Attacck patterni
-              (w/generate-windows attacks/udp-large-packets 1000 start-ts window-ms)
-              ;;  (w/generate-windows attacks/udp-empty-packets 1000 start-ts window-ms)
-              ;;  (w/generate-windows attacks/tcp-ack-anomaly 1000 start-ts window-ms)
-              (w/generate-windows attacks/subnet-carpet-bombing 1000 start-ts window-ms)
-              (w/generate-windows attacks/syn-flood 1000 start-ts window-ms)
-              (w/generate-windows attacks/icmp-flood 1000 start-ts window-ms)
-              (w/generate-windows attacks/udp-flood-mixed 1000 start-ts window-ms)
-              (w/generate-windows attacks/ntp-amplification 1000 start-ts window-ms)
-              (w/generate-windows attacks/ack-flood 1000 start-ts window-ms)
-              (w/generate-windows attacks/dns-amplification 1000 start-ts window-ms)
-              ;; Normanal saobracaj
-              (w/generate-windows normal/normal-web-traffic 1000 start-ts window-ms)
-              (w/generate-windows normal/normal-enterprise-traffic 1000 start-ts window-ms)
-              (w/generate-windows normal/normal-streaming-traffic 1000 start-ts window-ms)
-              (w/generate-windows normal/normal-dns-traffic 1000 start-ts window-ms)
-              (w/generate-windows normal/normal-email-traffic 1000 start-ts window-ms)
-              (w/generate-windows normal/normal-email-traffic 1000 start-ts window-ms))
-             (map #(vector (rand) %))
-             (sort-by first)
-             (map second))]
-    (export/write-csv samples "ddos_dataset.csv")
-    (println "Dataset generated: ddos_dataset.csv")))
+
+;; Ovo je bila inicijalna f-ja za generisanje dataseta
+;; (defn simple-dataset
+;;   [window-ms]
+;;   (let [start-ts (System/currentTimeMillis)
+;;         window-ms 5000
+;;         samples
+;;         (->> (concat
+;;               ;;  Attacck patterni
+;;               (w/generate-windows attacks/udp-large-packets 1000 start-ts window-ms)
+;;               ;;  (w/generate-windows attacks/udp-empty-packets 1000 start-ts window-ms)
+;;               ;;  (w/generate-windows attacks/tcp-ack-anomaly 1000 start-ts window-ms)
+;;               (w/generate-windows attacks/subnet-carpet-bombing 1000 start-ts window-ms)
+;;               (w/generate-windows attacks/syn-flood 1000 start-ts window-ms)
+;;               (w/generate-windows attacks/icmp-flood 1000 start-ts window-ms)
+;;               (w/generate-windows attacks/udp-flood-mixed 1000 start-ts window-ms)
+;;               (w/generate-windows attacks/ntp-amplification 1000 start-ts window-ms)
+;;               (w/generate-windows attacks/ack-flood 1000 start-ts window-ms)
+;;               (w/generate-windows attacks/dns-amplification 1000 start-ts window-ms)
+;;               ;; Normanal saobracaj
+;;               (w/generate-windows normal/normal-web-traffic 1000 start-ts window-ms)
+;;               (w/generate-windows normal/normal-enterprise-traffic 1000 start-ts window-ms)
+;;               (w/generate-windows normal/normal-streaming-traffic 1000 start-ts window-ms)
+;;               (w/generate-windows normal/normal-dns-traffic 1000 start-ts window-ms)
+;;               (w/generate-windows normal/normal-email-traffic 1000 start-ts window-ms)
+;;               (w/generate-windows normal/normal-email-traffic 1000 start-ts window-ms))
+;;              (map #(vector (rand) %))
+;;              (sort-by first)
+;;              (map second))]
+;;     (export/write-csv samples "ddos_dataset.csv")
+;;     (println "Dataset generated: ddos_dataset.csv")))
+
+
+
+(defn generate-complete-dataset [window-ms]
+  (w/generate-timeline window-ms)
+
+   [:normal {:normal-fn normal/normal-mixed-traffic
+             :num-windows 500}]
+   
+   [:attack {:attack-fn attacks/udp-large-packets
+             :attack-type :udp-flood-large}]
+   
+   [:normal {:normal-fn normal/normal-mixed-traffic
+             :num-windows 200}]
+   
+   [:attack {:attack-fn attacks/dns-amplification
+             :attack-type :dns-amplification}]
+   
+   [:normal {:normal-fn normal/normal-mixed-traffic
+             :num-windows 150}]
+   
+   [:campaign {:attack-fn attacks/syn-flood
+               :attack-type :syn-flood
+               :num-instances 4
+               :duration-hours 8}]
+   
+   [:normal {:normal-fn normal/normal-mixed-traffic
+             :num-windows 300}]
+    [:attack {:attack-fn attacks/icmp-flood
+              
+             :attack-type :icmp-flood}]
+   
+   [:normal {:normal-fn normal/normal-mixed-traffic
+             :num-windows 250}]
+   
+   [:attack {:attack-fn attacks/ack-flood
+             :attack-type :ack-flood}]
+   
+   [:normal {:normal-fn normal/normal-mixed-traffic
+             :num-windows 200}]
+   
+   [:attack {:attack-fn attacks/ntp-amplification
+             :attack-type :ntp-amplification}]
+   
+   [:normal {:normal-fn normal/normal-mixed-traffic
+             :num-windows 180}]
+   
+   [:attack {:attack-fn attacks/udp-flood-mixed
+             :attack-type :udp-flood-mixed}]
+   
+   [:normal {:normal-fn normal/normal-mixed-traffic
+             :num-windows 220}]
+   
+   [:attack {:attack-fn attacks/subnet-carpet-bombing
+             :attack-type :subnet-carpet-bombing}]
+   
+   [:normal {:normal-fn normal/normal-mixed-traffic
+             :num-windows 400}])
+  
+
+(defn -main [& args]
+  (let [window-ms 5000
+        data (do
+               (println "Generating dataset")
+               (generate-complete-dataset window-ms))]
+    (println data)
+    (export/write-csv data "new_ddos_dataset.csv")))
--- src/ddos/windowing.clj
+++ src/ddos/windowing.clj
@@ -1,7 +1,11 @@
 (ns ddos.windowing
-  (:require [ddos.random :as r]))
+  (:require [ddos.radnom :as r]
+            [incanter.core :as i]))
 
 ;; F-je koje dodaju vremenske serije podacima
+
+
+
 
 (defn add-window-metadata
   [sample window-id timestamp]
@@ -56,8 +60,8 @@
     :typical-duration 3600
     :long-attack-prob 0.35
     :long-duration-range [7200 21600]}
-   
-  ;;  Kod obicnog saobracaja se uzima 
+
+   ;;  Kod obicnog saobracaja se uzima 
    :normal
    {:min-duration 3600
     :max-duration 86400
@@ -67,14 +71,102 @@
 
 
 
+(defn define-duration
+
+  [attack-type]
+  (let [profile (get attack-pattern-duration attack-type)
+        {:keys [min-duration max-duration typical-duration long-attack-prob long-duration-range]} profile]
+
+    (if (< (rand) long-attack-prob)
+      ;; Dugacak napad
+      (let [[long-min long-max] long-duration-range]
+        (int (r/rand-uniform long-min long-max)))
+
+      ;; Normalni saobracaj
+      (let [dev (/ (- max-duration min-duration) 6.0)
+            duration (r/rand-normal typical-duration dev)]
+        (int (r/clamp duration min-duration max-duration))))))
+
+
+;; U buducnosti dodati talase napada posto moze i to da se desi 
+
+
+(defn generate-attack-windows [attack-fn attack-type start-timestap windows-ms]
+
+  (let [dur-sec (define-duration attack-type)
+        windows-num (max 1 (int (/ (* dur-sec 1000) windows-ms)))]
+    (vec
+     (for [i (range windows-num)]
+       (let [;;  time-sec (/ (* i windows-ms) 1000)
+             timestamp (+ start-timestap (* i windows-ms))]
+
+         (add-window-metadata
+          (attack-fn)
+          i
+          timestamp))))))
 
 ;; Izmenjeno je da se ova funkcija odnosi na normalni saobracaj
 (defn generate-windows-normal
-  [attack-fn n start-ts window-ms]
+  [attack-fn n start-timestamp window-ms]
   (map-indexed
    (fn [i _]
      (add-window-metadata
       (attack-fn)
       i
-      (+ start-ts (* i window-ms))))
+      (+ start-timestamp (* i window-ms))))
    (range n)))
+
+
+;; Funkcija za generisanje vektora sa svim opsezima instanci napada
+(defn generate-attack-vector
+
+  [attack-fn attack-type instances duration-hours start-timestap window-ms]
+  (let [duration-ms (* duration-hours 3600 1000)
+        spacing-ms (/ duration-ms instances)]
+    (vec
+     (mapcat
+      (fn [ind-instance]
+        (let [instance-start (+ start-timestap (* ind-instance spacing-ms))
+              windows (generate-attack-windows attack-fn attack-type instance-start window-ms)]
+
+          (map #(assoc %
+                       :instance-id ind-instance
+                       :vector-id (str attack-type "-vector"))
+               windows)))
+      (range instances)))))
+
+
+;; Funkcija za generisanja timeline odnosno organizovanog dataseta kod kojeg se napadi ne preklapaju
+(defn generate-timeline
+  [window-ms & attack-specs]
+  (let [start-ts (System/currentTimeMillis)]
+    (loop [curr-time start-ts
+           remaining-specs attack-specs
+           all-windows []]
+
+      (if (empty? remaining-specs)
+        all-windows
+        (let [[spec-type spec-data] (first remaining-specs)
+              new-window (case spec-type
+                           :attack
+                           (let [{:keys [attack-fn attack-type]} spec-data]
+                             (generate-attack-windows attack-fn attack-type curr-time window-ms))
+
+                           :vector
+                           (let [{:keys [attack-fn attack-type instances duration-hours]} spec-data]
+                             (generate-attack-vector attack-fn attack-type instances duration-hours curr-time window-ms))
+
+                           :normal
+                           (let [{:keys [normal-fn num-windows]} spec-data]
+                             (generate-windows-normal normal-fn num-windows curr-time window-ms)))
+
+              last-ts (if (empty? new-window)
+                        curr-time
+                        (:timestamp (last new-window)))
+
+              quiet-period-ms (* (r/rand-uniform 5 15) 60 1000) ;; Za pocetak je stavljeno da miran period bude od 5 do 15 min kasnije promentiti
+              next (+ last-ts window-ms quiet-period-ms)]
+
+          (recur next
+                 (rest remaining-specs)
+                 (concat all-windows new-window)))))))
